1a0f1e4197a96ea78daf994fdd9a89f484f099dc
YangRenRui <155858750+YANGRENRUIYRR@users.noreply.github.com>
2024-10-26 16:36:56 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -44,7 +44,7 @@
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
 import { Blog, Compass, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
-import { HelpSharp } from "@vicons/material";
+import { HelpSharp } from "@vicons/ionicons5";
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
